3ae9f1590e98fcd58847e67518652d9a07ad00ad
David Paz <david.paz@lbbw.de>
2021-05-16 09:50:18 +0200

    Added report paths to command starting jmeter

--- run.sh
+++ run.sh
@@ -5,6 +5,11 @@
 NAME="jmeter"
 JMETER_VERSION=${JMETER_VERSION:-5.4}
 IMAGE="justb4/jmeter:${JMETER_VERSION}"
+BUILD_DIR=build
+
+rm -rf ${BUILD_DIR}
 
 # run, network is the same as created by docker-compose
-docker run --network=influxdb_jmeter_network --rm --name ${NAME} -i -v ${PWD}:${PWD} -w ${PWD} ${IMAGE} $@
+docker run --network=influxdb_jmeter_network --rm \
+  --name ${NAME} -i -v ${PWD}:${PWD} -w ${PWD} ${IMAGE} \
+  -l ${BUILD_DIR}/test-plan.jtl -e -o ${BUILD_DIR} $@
